fe8e0ef533c82a4d212587e1f458825565b375ff
janlin <123davidbill@gmail.com>
2020-12-02 16:41:33 +0800

    githubpage

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:janlin002/burger.git master:gh-pages
+git push -f https://github.com/janlin002/burger.git master:gh-pages
 
 cd -
